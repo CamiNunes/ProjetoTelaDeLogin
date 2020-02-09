71c8c2bfcc3c758e2a4de5e19666147a3c22f4da
CamiNunes <cvn.camila@gmail.com>
2020-02-09 18:44:28 -0300

    Recuperando dados de um formulário com JavaScript;

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,28 @@
-console.log("Olá mundo");
+let email = 'cvn.camila@gmail.com';
+console.log(email);
+console.log('O seu email é: ' + email);
+
+email = 'teste@teste.com.br'
+console.log(`O seu email é: ${email}`);
+
+document.getElementById('btn-submit').addEventListener('click', e => {
+    console.log('O botão foi clicado!');
+});
+
+document.getElementById('form-login').addEventListener('mouseenter', e => {
+    console.log('O mousee stá sobre o formulário');
+});
+
+document.querySelector('#form-login').addEventListener('mouseleave', e => {
+    console.log('O mousee stá fora do formulário');
+});
+
+document.querySelector('#form-login').addEventListener('submit', e => {
+    e.preventDefault();
+    
+    let email = document.querySelector('#email').value;
+    let password = document.querySelector('#password').value;
+
+    console.log(email, password);
+});
+
